85067c934746f2e824d95671cbf6deb5c42e56c4
mariana.melo <mariana.melo@visma.com>
2025-04-14 17:50:25 +0100

    Add initial configuration files and basic tests for Magento site using Playwright

---
+++ playwright.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  use: {
+    baseURL: 'https://magento.softwaretestingboard.com/',
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure'
+  },
+  testDir: './tests',
+  timeout: 30000,
+});
